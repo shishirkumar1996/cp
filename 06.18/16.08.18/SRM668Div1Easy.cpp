856f5df7b339bc9ea2a9872438310930aed84ba9
shishirkumar1996 <shishirsharad@yahoo.com>
2018-06-16 15:51:15 +0530

    addition before atcoder competition on 16.06.18

---
+++ 06.18/16.08.18/SRM668Div1Easy.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define lld long long int
+#define faster ios_base::sync_with_stdio(false);cin.tie(0);
+#define vi vector< int >
+
+using namespace std;
+
+class PaintTheRoom{
+
+  public:
+    String canPaintEvenly(int R,int C,int K){
+      if(R%2== 0 || C%2 == 0 || k==1) return "Paint";
+      return "Cannot paint";
+    }
+};
